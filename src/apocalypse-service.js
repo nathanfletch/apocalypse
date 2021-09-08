38022d428aa8d283074f218f52a8db4c325fbbc9
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-08 16:30:45 -0700

    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>

--- src/apocalypse-service.js
+++ src/apocalypse-service.js
@@ -1,20 +1,21 @@
 export default class ApocalypseService {  
-  // static getComets() {
-  //   return new Promise(function(resolve, reject) {
-  //     let request = new XMLHttpRequest();
-  //     const url = ``;
-  //     request.onload = function() {
-  //       if (this.status === 200) {
-  //         resolve(request.response);
-  //       } else {
-  //         reject(request.response);
-  //       }
-  //     }
-  //     request.open("GET", url, true);
-  //     request.send();
-  //   });
-  // }
+  static getComets() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://ssd-api.jpl.nasa.gov/sentry.api`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
+  // static fireballs;
   static getFireballs() {
     return fetch(`https://ssd-api.jpl.nasa.gov/fireball.api`)
       .then(function(response) {
@@ -23,6 +24,7 @@ export default class ApocalypseService {
         }
         return response.json();
       })
+      //should this go in the other file? do i need to do more to handle this?
       .catch(function(error) {
         return error;
       })
--- src/index.js
+++ src/index.js
@@ -4,10 +4,54 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import ApocalypseService from "./apocalypse-service";
 
-$(document).ready(function() {
-  $("#fireball-select").click(function() {
-    const fireballs = ApocalypseService.getFireballs();
-    console.log(fireballs[[PromiseResult]]);
-    // const topTenFireballs = fireballs.filter(fireball => fireball.)
+$(document).ready(function () {
+  let fireballs;
+  let comets;
+
+  $("#fireball-select").click(function () {
+    ApocalypseService.getFireballs().then((data) => {
+      fireballs = data.data
+        .filter((fireball) => parseFloat(fireball[2]) >= 14)
+        .sort((a, b) => b[2] - a[2]);
+
+      displayTopTenFireballs();
+    });
   });
-})
+
+  function displayTopTenFireballs() {
+    const topTenHtml = fireballs.map((fireball, i) => {
+      return `<tr id="${i}">
+      <td>${fireball[2]}</td>
+      <td>${fireball[3]}</td>
+      <td>${fireball[5]}</td>
+      <td><button>Check distance</button></td>
+      </tr>`;
+    });
+    $("#fireball-table").append(topTenHtml);
+    $("#landing-screen").hide();
+    $("#fireball-screen").show();
+  }
+
+  $("#comet-select").click(function () {
+    let promise = ApocalypseService.getComets();
+    promise.then(function (response) {
+      comets = JSON.parse(response)
+        .data.sort((a, b) => b.ip - a.ip)
+        .splice(0, 10);
+      displayTopTenComets();
+    });
+  });
+
+  function displayTopTenComets() {
+    const topTenHtml = comets.map((comet) => {
+      return `<tr id="${comet.id}">
+          <td>${comet.fullname}</td>
+          <td>${comet.ip}</td>
+          <td>${comet.range}</td>
+        </tr>`;
+    });
+    $("#comet-table").append(topTenHtml);
+    $("#landing-screen").hide();
+    $("#comet-screen").show();
+  }
+});
