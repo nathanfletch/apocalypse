abb2d9cbb18ee5083d07311653ea805aac2b7ca2
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-09 10:26:50 -0700

    Add axios, error ui handling, and chained api call

--- src/index.js
+++ src/index.js
@@ -3,19 +3,31 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import ApocalypseService from "./apocalypse-service";
+import axios from "axios";
 
 $(document).ready(function () {
   let fireballs;
   let comets;
 
   $("#fireball-select").click(function () {
-    ApocalypseService.getFireballs().then((data) => {
-      fireballs = data.data
+    axios(`https://ssd-api.jpl.nasa.gov/fireball.api`)
+    // ApocalypseService.getFireballs()
+    .then((responseObject) => {
+      fireballs = responseObject.data.data
         .filter((fireball) => parseFloat(fireball[2]) >= 14)
         .sort((a, b) => b[2] - a[2]);
 
       displayTopTenFireballs();
-    });
+    }, (error) => {
+      $('#fireball-select').text(`There was an error processing your request: ${error}`);
+    })
+    .then(
+      axios(`http://api.openweathermap.org/geo/1.0/reverse?lat=${fireballs[0][3]}&lon=${fireballs[0][5]}&limit=1&appid=`).then(data => {
+        console.log(data);
+      })
+      //make a new api call
+    );
+    
   });
 
   function displayTopTenFireballs() {
@@ -37,7 +49,7 @@ $(document).ready(function () {
     promise.then(function (response) {
       comets = JSON.parse(response)
         .data.sort((a, b) => b.ip - a.ip)
-        .splice(0, 10);
+        .slice(0, 10);
       displayTopTenComets();
     });
   });
